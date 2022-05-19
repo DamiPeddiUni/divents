20dccc497d6d748b57c0622940cb76a3b941c9f9
ericamelotti <ericasofia.melotti@studenti.unitn.it>
2022-05-19 09:46:51 +0200

    Co-authored-by: Damiano Pedoni <DamiPeddiUni@users.noreply.github.com>

--- divents_server/usersManager.js
+++ divents_server/usersManager.js
@@ -1,7 +1,41 @@
 const User = require('./models/User')
 
 function checkUserAuth (req, res) { // dice se il profilo dell'utente è completo oppure no
-    User.find({auth_id :req.params.id})
+    User.findOne({auth_id :req.params.id})
+    .then((result) => {
+        if (result){
+            var response = {
+                profile_completed: result.profile_completed
+            }
+            
+            res.send(JSON.stringify(response))
+        }else{
+            var response = {
+                profile_completed: false
+            }
+            res.send(JSON.stringify(response))
+        }
+        
+    })
+    .catch((err) => {
+        console.log(err)
+        res.send(err)
+    })
+}
+
+function registerUser (req, res) { // inserisco i dati mancanti nel profilo dell'utente
+    
+    const user = new User({
+        auth_id: req.body.auth_id,
+        name : req.body.name,
+        email : req.body.email,
+        type : req.body.type,
+        profile_photo : req.body.profile_photo,
+        brief_presentation : req.body.brief_presentation,
+        location : req.body.location,
+        profile_completed : true
+    })
+    user.save()
     .then((result) => {
         if (result.length > 0){
             var response = {
@@ -14,29 +48,12 @@ function checkUserAuth (req, res) { // dice se il profilo dell'utente è complet
             }
             res.send(JSON.stringify(response))
         }
-        
     })
     .catch((err) => {
+        console.log(err)
         res.send(err)
     })
-}
-
-function registerUser (req, res) { // inserisco i dati mancanti nel profilo dell'utente
-    res.send("registerUser")
-    User.findByIdAndUpdate(req.body.id, { 
-        name : req.body.name, 
-        type : req.body.type,
-        profile_photo : req.body.profile_photo,
-        brief_presentation : req.body.brief_presentation,
-        location : req.body.location,
-        profile_completed : true
-    })
-    .then((result) => {
-        res.send(result);
-    })
-    .catch((err) => {
-        res.send(err)
-    })
+    
 }
 
 
