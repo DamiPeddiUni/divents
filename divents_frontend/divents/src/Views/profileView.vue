22af07abef04982421cc53c21a1caf1f59f50579
adomonti <adomonti21@gmail.com>
2022-05-30 18:27:12 +0200

    Aggiunta visualizzazione eventi creati dall'utente

--- divents_frontend/divents/src/Views/profileView.vue
+++ divents_frontend/divents/src/Views/profileView.vue
@@ -29,6 +29,23 @@
                     </div>
                 </div>
                 <div>Events:</div>
+                <div class="space"></div>
+                <div class="events-container">
+                    <div v-for="(event, index) in events" :key="index">
+                    <a :href="'/event/' + event._id" class="event-card">
+                        <div class="event-card-image" :style="{ 'background-image': 'url(' + event.photos[0] + ')' }"></div>
+                        <div class="event-card-text">
+                        <div class="event-card-header">
+                            <div class="event-card-location">{{event.location}}</div>
+                            <div class="event-card-date">{{new Date(event.date).toLocaleDateString("it-IT")}}</div>
+                        </div>
+                        <div class="event-card-title">{{event.title}}</div>
+                        <div class="event-card-subtitle">{{event.subtitle}}</div>
+                        <div class="event-card-subscribers">{{event.subscribers.length}} people take part</div>
+                        </div>
+                    </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -43,6 +60,7 @@
             return{
                 user_id: "",
                 user_details: {},
+                events: {},
             };
         },
         methods: {
@@ -61,6 +79,7 @@
                     DataService.getUserDetails(result.data)
                     .then(result =>{
                         this.user_details=result.data
+                        this.getUserEvents()
                     })
                     .catch(err =>{
                         console.log("Errore nel reperimento dei dettagli dell'utente")
@@ -70,6 +89,16 @@
                     console.log("Errore nella conversione dell'auth id")
                 })
                 
+            },
+            getUserEvents(){
+                DataService.getSubscriptionsEvents(JSON.stringify(this.user_id))
+                .then(response => {
+                        this.events = response.data
+                    })
+                    .catch(error => {
+                        console.log("errore")
+                        console.log(error)
+                })
             }
         },
         mounted(){
@@ -127,5 +156,96 @@
     .descr{
         padding-top: 20px;
         padding-bottom: 20px;
+    }
+    .events-container{
+        display: grid;
+        gap: 10px;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    @media screen and (max-width: 1024px){
+        .events-container{
+        grid-template-columns: 1fr 1fr 1fr;
+        }
+    }
+    @media screen and (max-width: 640px){
+        .events-container{
+        grid-template-columns: 1fr 1fr;
+        }
+    }
+    @media screen and (max-width: 480px){
+        .events-container{
+        grid-template-columns: 1fr;
+        }
+    }
+    .event-card{
+        width: 100%;
+        height: 310px;
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 5px;
+        text-decoration: none;
+        display: block;
+        color: black;
+    }
+    .event-card-image{
+        width: 100%;
+        height: 150px;
+        border-radius: 10px 10px 0 0;
+        background: rgba(0, 0, 0, 0.2);
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    .event-card-text{
+        padding: 10px;
+    }
+    .event-card-header{
+        height: 16px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-bottom: 5px;
+    }
+    .event-card-location{
+        font-size: 12px;
+        color: #1B98E0;
+    }
+    .event-card-date{
+        font-size: 12px;
+    }
+    .event-card-title{
+        height: 40px;
+        font-size: 20px;
+        font-weight: bold;
+        line-height: 20px;
+        margin-bottom: 5px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+    }
+    .event-card-subtitle{
+        height: 48px;
+        font-size: 12px;
+        margin-bottom: 10px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+    }
+    .event-card-subscribers{
+        height: 16px;
+        font-size: 12px;
+        color: #1B98E0;
+        font-weight: bold;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+    }
+    .space{
+        padding: 5px;
     }
 </style>
